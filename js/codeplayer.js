4c10acd0b89514b1b273400f4227e45b1ead134a
Clare <chblackburne@gmail.com>
2017-12-30 14:52:31 +0000

    refactor jquery file to include update function

--- js/codeplayer.js
+++ js/codeplayer.js
@@ -1,30 +1,50 @@
 
-$(function(){
-  $(".toggleButton").hover( function (){
-    $(this).addClass("highlightedButton");
-  }, function(){
-    $(this).removeClass("highlightedButton");
-  });
 
-  $(".toggleButton").click(function(){
-    $(this).toggleClass("active");
-    $(this).removeClass("highlightedButton");
-    var panelID = $(this).attr("id") + "Panel";
-    $("#" + panelID).toggleClass("hidden");
-    var numofActivePanels = 4 - $(".hidden").length;
-    $(".panel").width(($(window).width() / numberofActivePanels) - 10);
+function updateOutput() {
 
-  });
+  $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
 
-  $(".panel").height($(window).height() - $("#header").height() - 15);
+  document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
 
-  $(".panel").width(($(window).width() / 2) - 10);
 
-  $("iframe").contents().find("html").html($("#htmlPanel").val());
 
-// updates automatically
-  $("textarea").on("change keyup paste", function(){
-    $("iframe").contents().find("html").html($("#htmlPanel").val());
+}
 
-  });
+$(".toggleButton").hover(function() {
+
+  $(this).addClass("highlightedButton");
+
+}, function() {
+
+  $(this).removeClass("highlightedButton");
+
+});
+
+$(".toggleButton").click(function() {
+
+  $(this).toggleClass("active");
+
+  $(this).removeClass("highlightedButton");
+
+  var panelId = $(this).attr("id") + "Panel";
+
+  $("#" + panelId).toggleClass("hidden");
+
+  var numberOfActivePanels = 4 - $('.hidden').length;
+
+  $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
+
+})
+
+$(".panel").height($(window).height() - $("#header").height() - 15);
+
+$(".panel").width(($(window).width() / 2) - 10);
+
+updateOutput();
+
+$("textarea").on('change keyup paste', function() {
+
+  updateOutput();
+
+
 });
