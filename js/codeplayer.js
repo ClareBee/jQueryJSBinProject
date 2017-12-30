3cf500a03f8da27e5b46012985e2bd1eea8ea0a2
Clare <chblackburne@gmail.com>
2017-12-30 14:00:32 +0000

    Adds jQuery to buttons

---
+++ js/codeplayer.js
@@ -0,0 +1,13 @@
+
+$(function(){
+  $(".toggleButton").hover( function (){
+    $(this).addClass("highlightedButton");
+  }, function(){
+    $(this).removeClass("highlightedButton");
+  });
+
+  $(".toggleButton").click(function(){
+    $(this).toggleClass("active");
+    $(this).removeClass("highlightedButton");
+  });
+});
